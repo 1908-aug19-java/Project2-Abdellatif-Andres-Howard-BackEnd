7335dad2e6ceadc70a2a3c326f57637046d74053
Abdellatif Elmansouri <axelmansouri1@gmail.com>
2019-10-02 21:41:06 -0400

    pom edited for testing

--- Pokemon-app-Hibernate/src/main/java/com/revature/project2/dao/AccountInfoDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature.project2.dao;
-
-
-
-import com.revature.project2.model.AccountInfo;
-
-public interface AccountInfoDao {
-	public AccountInfo getAccountInfoById(int id);
-}
--- Pokemon-app-Hibernate/src/main/java/com/revature/project2/dao/ItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature.project2.dao;
-
-import java.util.List;
-
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
-
-public interface ItemDao {
-	public List<Item> getItemById(int id);
-}
