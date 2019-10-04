620c3935b09dd4ae46ebb8fd510df50a2362ca2a
ABDELLATIF ELMANSOURI <45475426+VHDLNER@users.noreply.github.com>
2019-10-03 20:29:31 -0400

    Update LoginController.java

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -14,7 +14,7 @@ import com.revature.models.Trainer;
 import com.revature.services.TrainerService;
 
 @RestController 
-@CrossOrigin(maxAge = 3600)
+@CrossOrigin()
 @RequestMapping("/login")
 public class LoginController 
 {	private static Logger log = Logger.getRootLogger();
